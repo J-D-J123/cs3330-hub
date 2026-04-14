2e492400b5aa2e481246d53f8fc6d6889cdf3fd9
J-D-J123 <jdjohnson1506@gmail.com>
2026-04-14 14:07:58 -0500

    TrapTest.java init
    - set up test/ folder
    - set up TrapTest.java folder with testing methods

---
+++ haunted-university-basement/src/test/TrapTest.java
@@ -0,0 +1,28 @@
+/**
+* File: 		TrapTest.java 
+* Author:	Joey Johnson
+* Desc:		to test the Trap.java class logic in package 
+* 				com.example.haunted.model
+*/ 
+
+package com.example.haunted.model
+
+public class MainTest {
+
+    @Test
+    /**
+     * makeTrap makes the trap 
+     */
+    void makeTrap() {
+
+    }
+    
+    @Test 
+    /**
+     * getName() gets the name of the trap 
+     */
+    String getName() {
+    	
+    }
+}
+

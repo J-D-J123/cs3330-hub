b902eb0e1c82bf41eea843bebaf6d18dfd80c163
Tucker <151984953+TuckerPMIZ@users.noreply.github.com>
2026-04-16 18:55:44 -0500

    revised comments
    Updated comments and formatting in TestMonster class.

--- haunted-university-basement/src/test/java/com/example/haunted/model/TestMonster.java
+++ haunted-university-basement/src/test/java/com/example/haunted/model/TestMonster.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 /* Name: Tucker Potts
-*	This function tests the monster class.
+*	This test class tests the monster class.
 *
 */
 public class TestMonster {
